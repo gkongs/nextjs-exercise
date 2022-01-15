73f4fdac7328c07e19bfd2f8f2424305a0735f48
jcy <poiuytasr@gmail.com>
2022-01-15 22:21:51 +0900

    Layout 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-// import Layout from '../components/Layout';
+import Layout from '../components/Layout';
 import { GlobalStyle } from '../styles/global';
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
       <GlobalStyle />
-      {/* <Layout> */}
-      <Component {...pageProps} />
-      {/* </Layout> */}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </div>
   );
 }
